b44f1058495dc6db298f2da016fdfd48d0709635
joaonetoeng <joaonetu@live.com>
2020-06-23 10:35:34 -0300

    Primeiro Commit - CORRIGIDO

---
+++ pypro/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+
+from pypro.base.views import home
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home)
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns.append(
+        path('__debug__/', include(debug_toolbar.urls))
+    )
